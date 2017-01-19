7cb77c3c15d4e5eb205403434b2e9e1c36ce6c71
Antonio Sun <asun@ats.l5.ca>
2017-01-19 13:02:06 -0500

    - [+] init code

---
+++ json01-des01/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class ProgTest
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
