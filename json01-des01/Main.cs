67f13ed5ddc27f0cc070764a0aec97e5409c18cc
Antonio Sun <asun@ats.l5.ca>
2017-01-19 15:19:14 -0500

    - [*] try Deserialize Object from json with More Fields

--- json01-des01/Main.cs
+++ json01-des01/Main.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -30,7 +30,7 @@ public class Account
 
     public static void DeserializeObject()
     {
-        string json = @"{
+        string json1 = @"{
           'Email': 'james@example.com',
           'Active': true,
           'CreatedDate': '2013-01-20T00:00:00Z',
@@ -39,13 +39,13 @@ public class Account
             'Admin'
           ]
         }";
-        Account account = JsonConvert.DeserializeObject<Account>(json);
+        Account account = JsonConvert.DeserializeObject<Account>(json1);
         Console.WriteLine(account.Email);
 
         // Serialize an Object
         // http://www.newtonsoft.com/json/help/html/SerializeObject.htm
-        string json2 = JsonConvert.SerializeObject(account, Formatting.Indented);
-        Console.WriteLine(json2);
+        string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+        Console.WriteLine(json);
         // {
         //   "Email": "james@example.com",
         //   "Active": true,
@@ -55,6 +55,28 @@ public class Account
         //     "Admin"
         //   ]
         // }
+
+        string json2 = @"{
+          'Email': 'james@example.com',
+          'Active': true,
+          'CreatedDate': '2013-01-20T00:00:00Z',
+          'Some': 1,
+          'More': 2,
+          'Fields': 'like this',
+          'Roles': [
+            'User',
+            'Admin'
+          ]
+        }";
+        Account account2 = JsonConvert.DeserializeObject<Account>(json2);
+        if (account == account2) Console.WriteLine("Same"); 
+        else Console.WriteLine("Not the same");
+        if (account.Equals(account2)) Console.WriteLine("Same"); 
+        else Console.WriteLine("Not the same");
+        string json2r = JsonConvert.SerializeObject(account, Formatting.Indented);
+        Console.WriteLine(json2r);
+        if (json == json2r) Console.WriteLine("Same"); 
+        else Console.WriteLine("Not the same");
     }
 }
 
