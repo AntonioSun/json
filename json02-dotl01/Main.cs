43db407346d82459847e3562c73be6a533999d0c
Antonio Sun <asun@ats.l5.ca>
2017-01-20 14:20:35 -0500

    - [*] add 2 DotLiquid demos

--- json02-dotl01/Main.cs
+++ json02-dotl01/Main.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
+using DotLiquid;
 
 class ProgTest
 {
     static void Main(string[] args)
     {
+        Demo0();
+        DemoFilter();
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
     }
+
+    static void Demo0()
+    {
+        var template = Template.Parse("Hello, {{ name }}!");
+        var result = template.Render(Hash.FromAnonymousObject(new { name = "World" }));
+        Console.WriteLine(result);
+    }
+
+    static void DemoFilter()
+    {
+        var template = Template.Parse("Hello, {{ name | upcase | escape }}!");
+        var result = template.Render(Hash.FromAnonymousObject(new { name = "<World>" }));
+        Console.WriteLine(result);
+    }
+
+
 }
