b690d7f0147380d364ce463a5693ab1c06441c18
Antonio Sun <asun@ats.l5.ca>
2017-01-23 16:36:16 -0500

    - [+] add json03-all01 project, JsonTest1 good

---
+++ json03-all01/Main.cs
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////////////
+// Porgram: 
+// Purpose: C# Demo
+// authors: Antonio Sun (c) 2017, All rights reserved
+// Credits: as credited below
+////////////////////////////////////////////////////////////////////////////
+
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO; // TextWriter
+
+using DotLiquid;
+
+
+class ProgTest
+{
+    static void Main(string[] args)
+    {
+        Demo0.Test();
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
